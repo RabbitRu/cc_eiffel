e0994c4822070d876a030d16c0f5bfde07f3e35e
Егор Домнин <edomnin@ivi.ru>
2019-08-12 01:13:16 +0300

    Добавил парсинг Notes

---
+++ Syntaxer/Nodes/ClassNode.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Syntaxer.Nodes
+{
+    public class ClassNode : BaseNode
+    {
+        public string Name;
+        public string Obsolete;
+        public List<string> Inheritance;
+        public List<string> Creators;
+        public List<string> Converters;
+        public List<string> Features;
+        public NoteNode Notes;
+        public List<string> Invariant;
+
+        public ClassNode()
+        {
+            Inheritance = new List<string>();
+            Creators = new List<string>();
+            Converters = new List<string>();
+            Features = new List<string>();
+            Notes = new NoteNode();
+            Invariant = new List<string>();
+        }
+
+        public class NoteNode :BaseNode
+        {
+            public List<string> NoteNames;
+            public List<List<string>> NoteContents;
+
+            public NoteNode()
+            {
+                NoteNames = new List<string>();
+                NoteContents = new List<List<string>>();
+            }
+
+            public void Add(string name, List<string> content)
+            {
+                NoteNames.Add(name);
+                NoteContents.Add(content);
+            }
+        }
+
+    }
+}
--- Syntaxer/Syntaxer.cs
+++ Syntaxer/Syntaxer.cs
@@ -19,11 +19,14 @@ namespace Syntaxer
         private Token getNextToken()
         {
             if (currentTokenNumber >= source.Count)
+            {
+                currenToken = null;
                 return null;
-            var x = source[currentTokenNumber];
+            }
+
             currenToken = source[currentTokenNumber];
             currentTokenNumber++;
-            return x;
+            return currenToken;
         }
 
         public void BuildAST(List<Token> tokens)
@@ -51,12 +54,134 @@ namespace Syntaxer
                 case TokenType.Identifier:
                     returnValue = ParseIdentifier(currenToken);
                     break;
+                case TokenType.ReservedWord:
+                    returnValue = ParseReservedWords(currenToken);
+                    break;
                 default:
                     getNextToken();
                     break;
             }
 
             return returnValue;
+        }
+
+        private BaseNode ParseReservedWords(Token token)
+        {
+            BaseNode returnValue = null;
+            ClassNode classWNotes = new ClassNode();
+            switch (token.Value)
+            {
+                case "class":
+                    getNextToken();
+                    returnValue = ParseClass(currenToken, classWNotes);
+                    break;
+                case "note":
+                    getNextToken();
+                    classWNotes = new ClassNode();
+                    ParseNote(currenToken, classWNotes.Notes);
+                    break;
+                default:
+                    getNextToken();
+                    break;
+            }
+
+            return returnValue;
+        }
+
+        private BaseNode ParseClass(Token token, ClassNode classWNotes = null)
+        {
+            ClassNode classN;
+            classN = classWNotes ?? new ClassNode();
+
+            while (currenToken != null && currenToken.Value != "end")
+            {
+                switch (currenToken.Value)
+                {
+                    case "obsolete":
+                        getNextToken();
+                        break;
+                    case "note":
+                        getNextToken();
+                        ParseNote(currenToken, classN.Notes);
+                        break;
+                    case "inheritance":
+                        getNextToken();
+                        break;
+                    case "creators":
+                        getNextToken();
+                        break;
+                    case "converters":
+                        getNextToken();
+                        break;
+                    case "features":
+                        getNextToken();
+                        break;
+                    case "invariant":
+                        getNextToken();
+                        break;
+                    default:
+                        getNextToken();
+                        break;
+                }
+
+            }
+
+            return classN;
+        }
+
+        private void ParseNote(Token token, ClassNode.NoteNode notes)
+        {
+            string nName;
+            List<string> nContent;
+            while (currenToken.Type == TokenType.Identifier)
+            {// nName: nContent
+                if (currenToken.Type==TokenType.Identifier)
+                {
+                    nName = currenToken.Value;
+                }
+                else
+                {
+                    Debug.Print("Note parse error1");
+                    return;
+                }
+                getNextToken();
+
+                if (currenToken.Type != TokenType.Delimeter || currenToken.Value != ":")
+                {
+                    return;
+                }
+
+                getNextToken();
+                nContent = new List<string>();
+
+                while (true)
+                {
+                    if (currenToken.Type == TokenType.Constant || currenToken.Type == TokenType.Identifier)
+                    {
+                        nContent.Add(currenToken.Value);
+                    }
+                    else
+                    {
+                        Debug.Print("Note parse error2");
+                        break;
+                    }
+
+                    getNextToken();
+
+                    if (currenToken.Type == TokenType.Delimeter && currenToken.Value == ",")
+                    {
+                        nContent.Add(currenToken.Value);
+                        getNextToken();
+                    }
+                    else
+                    {//Норма
+                        break;
+                    }
+
+                }
+
+                notes.Add(nName, nContent);
+            }
         }
 
         private BaseNode ParseIdentifier(Token identidier)
